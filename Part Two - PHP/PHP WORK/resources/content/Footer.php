6a81a1c96652aec7610b40fe19cddd2a93439343
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2022-03-06 17:05:13 +0000

    Delete Part Two

--- Part Two - PHP/PHP WORK/resources/content/Footer.php
+++
@@ -1,7 +0,0 @@
-
-
-<div class="jumbotron text-center" style="margin-bottom:0">
-    <p>Copyright © 2021 School of Engineering, Computing & Mathematics with University of Plymouth. All rights reserved.</p>
-</div>
-
-<?php
